f793814531cb794cc760e0426469a8429facbd67
bli013 <btli@ymail.com>
2020-03-18 19:02:38 -0700

    Updates

---
+++ precache-manifest.cc65de82b5469696630b7dd147a2dce1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d198dad0e0c1cb97605b69bff3365db",
+    "url": "/tsz-portfolio/index.html"
+  },
+  {
+    "revision": "6e036dac4abba9e13eba",
+    "url": "/tsz-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "13f3afe336564065cc44",
+    "url": "/tsz-portfolio/static/js/2.0df278d4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tsz-portfolio/static/js/2.0df278d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e036dac4abba9e13eba",
+    "url": "/tsz-portfolio/static/js/main.61faeb35.chunk.js"
+  },
+  {
+    "revision": "73d94f4edb111adfc27e",
+    "url": "/tsz-portfolio/static/js/runtime-main.6d7b6edc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tsz-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
